dcb78acb7edacfbc21f37ea41309bb162f551c42
Pankaj Sharma <pankaj.sharma@appventurez.com>
2022-01-02 19:23:05 +0530

    update skills section

--- src/component/Skills/Skills.js
+++ src/component/Skills/Skills.js
@@ -4,19 +4,20 @@ import "./Skills.css";
 const Skills = () => {
   return (
     <section id="skills" className="skills">
-      <h2> My Skills </h2>
+      <h2>✨ My Skills ✨</h2>
 
       <ul className="skills-list">
         <li className="md-chip md-chip-hover"> Node JS </li>
-        <li className="md-chip md-chip-hover"> PHP </li>
-        <li className="md-chip md-chip-hover"> JS </li>
+        <li className="md-chip md-chip-hover"> Git </li>
+        <li className="md-chip md-chip-hover"> Javascript </li>
         <li className="md-chip md-chip-hover"> HTML </li>
         <li className="md-chip md-chip-hover"> CSS </li>
         <li className="md-chip md-chip-hover"> Codeigniter </li>
         <li className="md-chip md-chip-hover"> Express </li>
+        <li className="md-chip md-chip-hover"> MongoDB </li>
         <li className="md-chip md-chip-hover"> Socket.IO </li>
-        <li className="md-chip md-chip-hover"> MySql </li>
-        <li className="md-chip md-chip-hover"> AngularJS </li>
+        <li className="md-chip md-chip-hover"> SQL </li>
+        <li className="md-chip md-chip-hover"> API </li>
         <li className="md-chip md-chip-hover"> React </li>
         <li className="md-chip md-chip-hover"> jQuery </li>
       </ul>
